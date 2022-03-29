b83e55c67e14ac3fe9003d7f98cb631f4444c305
Shahroz Ansari <shahrozansari@Shahrozs-MacBook-Air.local>
2022-03-29 11:53:39 +0530

    Temp 2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/game2",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+      {
+        source: "/game3",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "gamezop.com",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["picsum.photos"],
---
+++ pages/game3.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+
+const Game3: NextPage<PageProps> = () => {
+  return (
+    <div>
+      <h1>Game</h1>
+      <iframe
+        id="game"
+        style={{ overflow: "hidden", height: "400px" }}
+        src="https://www.gamezop.com/g/HJP4afkvqJQ?id=4419"
+      ></iframe>
+    </div>
+  );
+};
+
+interface PageProps {}
+
+export default Game3;
